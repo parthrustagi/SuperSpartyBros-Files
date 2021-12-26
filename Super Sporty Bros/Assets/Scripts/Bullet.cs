481e162a4ba04ab5800bd5d5e5fd559253300d8e
parthrustagi <parthrustagi1@gmail.com>
2021-12-26 02:27:06 -0500

    First Commit

---
+++ Super Sporty Bros/Assets/Scripts/Bullet.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Bullet : MonoBehaviour {
+
+    public float speed = 20f;
+    public Rigidbody2D rb;
+
+	// Use this for initialization
+	void Start ()
+    {
+        rb.velocity = transform.right * speed;
+	}
+
+    void OnTriggerEnter2D (Collider2D other)
+    {
+        Enemy enemy = other.GetComponent<Enemy>();
+        if(enemy != null)
+        {
+            enemy.Destroy(100);
+        }
+        if (other.gameObject.tag == "Enemy")
+        {
+            Destroy(gameObject);
+        }
+    }
+
+}
